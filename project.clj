ddbbbe5e312755cbeff64494eb56043e5a1d1084
AndyL <core.async@gmail.com>
2016-07-17 14:27:34 -0700

    initial

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject my "0.1.0"
+  :description "neater-git"
+  :repositories [["conjars" "http://conjars.org/repo"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/algo.generic "0.1.2"]
+                 [clj-jgit "0.8.9"]])
